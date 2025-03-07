e9617a54347e2d213670f7d206a31afae4da6cee
ramonpr <pozueloruizramon@gmail.com>
2025-03-07 02:26:21 +0100

    Added icon and Expertise, check App.tsx for more changes

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,44 +1,68 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faDocker, faPython  } from '@fortawesome/free-brands-svg-icons';
+import { faPlane, faSatellite, faJetFighter, faEarth, faBrain, faSquarePollVertical} from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
-    "Python",
-    "SQL",
-    "PostgreSQL",
-    "Postman"
+    // "React",
+    // "TypeScript",
+    // "JavaScript",
+    // "HTML5",
+    // "CSS3",
+    // "SASS",
+    // "Flask",
+    // "Python",
+    // "MATLAB",
+    // "Fortran",
+    // "C++",
+    // "SQL",
+    // "PostgreSQL",
+    // "Postman"
+    "Calculus",
+    "Physics",
+    "Thermodynamics",
+    "Fluid Dynamics",
+    "Solid Mechanics",
+    "Propulsion",
+    "Aerodynamics",
+    "Aircraft Design",
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    // "Git",
+    // "GitHub Actions",
+    // "Docker",
+    // "AWS",
+    // "Azure",
+    // "Linux",
+    // "Snowflake",
+    // "Pandas",
+    // "Selenium",
+    "Fortran",
+    "C++",
+    "Python",
+    "MATLAB",
+    "Pytorch",
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    // "OpenAI",
+    // "Groq",
+    // "LangChain",
+    // "Qdrant",
+    // "Hugging Face",
+    // "LlamaIndex",
+    // "Streamlit",
+    "CAD",
+    "CFD",
+    "FEA",
+    "Catia",
+    "FreeCAD",
+    "Patran",
+    "Nastran",
 ];
 
 function Expertise() {
@@ -47,10 +71,15 @@ function Expertise() {
         <div className="skills-container">
             <h1>Expertise</h1>
             <div className="skills-grid">
+
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <div style={{ display: 'flex', gap: '10px' }}>
+                        <FontAwesomeIcon icon={faPlane} size="3x"/>
+                        <FontAwesomeIcon icon={faEarth} size="3x"/>
+                        <FontAwesomeIcon icon={faSatellite} size="3x"/>
+                    </div>
+                    <h3>Aerospace</h3>
+                    <p>I have studied the bachelor of aeronautic engineering, providing a broad knowledge in engineering. From solid and fluid dynamics, propulsion and even electronics. </p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -60,9 +89,16 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <div style={{ display: 'flex', gap: '10px' }}>
+                        <FontAwesomeIcon icon={faBrain} size="3x"/>
+                        <FontAwesomeIcon icon={faSquarePollVertical} size="3x"/>
+                        <FontAwesomeIcon icon={faPython} size="3x"/>
+                    </div>
+                    <h3>Data Science / Research and AI</h3>
+                    <p> As a PhD and PostDoc researcher we develop the ability to learn new topics and tools,
+                        such as programming in different languages, performing simulations in large clusters and analysing data. 
+                        Sometimes a direct algorithmic approach is needed and sometimes the topic required the use of AI.
+                    </p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -73,8 +109,11 @@ function Expertise() {
 
                 <div className="skill">
                     <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <h3>Mechanical Design</h3>
+                    <p> Looking for a way to contribute to society, I try to keep updated with Mechanical design, and learn  new 
+                        topics where I can use my knowledge in a more applied way. Designing pieces or mechanisms that can be 3D printed,
+                        learning a little about embedded systems and electronics, or even reusing old phones by creating my custom Android apps.
+                    </p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
@@ -82,6 +121,8 @@ function Expertise() {
                         ))}
                     </div>
                 </div>
+
+               
             </div>
         </div>
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,22 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          {/* <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" /> */}
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Ramon-PR" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/ramon-pozuelo-ruiz/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Ramón Pozuelo</h1>
+          {/* <p>MSc Aerospace Eng. | PhD Eng. Mechanics | PostDoc ML & AI</p> */}
+          {/* <p>PhD Eng. Mechanics | MSc Aerospace Eng. | PostDoc ML & AI</p> */}
+          <p>Aerospace & Mechanical Engineer | ML & AI</p>
+
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Ramon-PR" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/ramon-pozuelo-ruiz/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
